081cac077cdbad149de0d15886c3aaa432acc56f
IsmailAlamKhan <mdismailalamkhan@gmail.com>
2021-05-08 17:08:24 +0600

    added from json on IsmailFormFieldOption

--- lib/src/forms/item.dart
+++ lib/src/forms/item.dart
@@ -10,6 +10,10 @@ class IsmailFormFieldOption<T> extends StatelessWidget {
   final T value;
   final Widget? child;
   final String? text;
+  IsmailFormFieldOption.fromJson(Map<String, dynamic> map)
+      : value = map['value'],
+        child = null,
+        text = map['text'] ?? map['Text'];
   @override
   Widget build(BuildContext context) {
     return child ?? Text(text ?? value.toString());
