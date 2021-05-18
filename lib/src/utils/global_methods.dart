bacececaf026874f6a1d5f7c5f5b30b28f57709c
IsmailAlamKhan <mdismailalamkhan@gmail.com>
2021-05-18 17:53:23 +0600

    added focusChange and crossFadeState

--- lib/src/utils/global_methods.dart
+++ lib/src/utils/global_methods.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+
 /// Gets weither the platform will use Material or not
 bool get isMaterial =>
     Platform.isAndroid ||
@@ -10,3 +12,17 @@ bool get isMaterial =>
 /// Gets if the platform is desktop or not
 bool get isDesktop =>
     Platform.isLinux || Platform.isWindows || Platform.isMacOS;
+void focusChange({
+  required BuildContext context,
+  required FocusNode focusNode,
+}) {
+  FocusScope.of(context).requestFocus(focusNode);
+}
+
+CrossFadeState crossFadeState(bool cond) {
+  if (cond) {
+    return CrossFadeState.showFirst;
+  } else {
+    return CrossFadeState.showSecond;
+  }
+}
