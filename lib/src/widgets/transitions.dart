662db96c5c9910181cfd593e29ffcca49537d8cd
Md. Shariful Alam Khan <mdismailalamkhan@gmail.com>
2021-06-18 15:55:13 +0600

    added the ControlAnimationWithCondition into transitions

--- lib/src/widgets/transitions.dart
+++ lib/src/widgets/transitions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../src.dart';
+
 /// Base Class of the animated widgets I provided
 /// This takes Animation<double> to do the animation
 /// provides a progress variable which will give the correct animation
@@ -143,6 +145,52 @@ class FadeSizeTransition extends AnimatedWidget {
         sizeFactor: _sizeFactor,
         child: child,
       ),
+    );
+  }
+}
+
+typedef ControlAnimationWithConditionBuilder = Widget Function(
+  BuildContext context,
+  Widget? child,
+  Animation<double> animation,
+);
+
+class ControlAnimationWithCondition extends StatefulWidget {
+  const ControlAnimationWithCondition({
+    Key? key,
+    required this.builder,
+    this.child,
+    required this.condition,
+    this.duration = const Duration(milliseconds: 500),
+  }) : super(key: key);
+  final ControlAnimationWithConditionBuilder builder;
+  final Widget? child;
+  final bool condition;
+  final Duration duration;
+  @override
+  ControlAnimationWithConditionState createState() =>
+      ControlAnimationWithConditionState();
+}
+
+class ControlAnimationWithConditionState
+    extends AnimationControllerState<ControlAnimationWithCondition> {
+  @override
+  Duration get duration => widget.duration;
+  @override
+  Widget build(BuildContext context) {
+    if (widget.condition) {
+      animation.forward();
+    } else {
+      animation.reverse();
+    }
+    return AnimatedBuilder(
+      builder: (context, child) => widget.builder(
+        context,
+        child,
+        animation,
+      ),
+      animation: animation,
+      child: widget.child,
     );
   }
 }
