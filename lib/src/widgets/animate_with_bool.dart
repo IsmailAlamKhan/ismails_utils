f75a88f8fffafb6fa0ef8c117331543b4776e378
Md. Shariful Alam Khan <mdismailalamkhan@gmail.com>
2021-06-18 14:57:51 +0600

    Create animate_with_bool.dart

---
+++ lib/src/widgets/animate_with_bool.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import '../src.dart';
+
+typedef ControlAnimationWithConditionBuilder = Widget Function(
+  BuildContext context,
+  Widget? child,
+  Animation<double> animation,
+);
+
+class ControlAnimationWithCondition extends StatefulWidget {
+  const ControlAnimationWithCondition({
+    Key? key,
+    required this.builder,
+    this.child,
+    required this.condition,
+    this.duration = const Duration(milliseconds: 500),
+  }) : super(key: key);
+  final ControlAnimationWithConditionBuilder builder;
+  final Widget? child;
+  final bool condition;
+  final Duration duration;
+  @override
+  ControlAnimationWithConditionState createState() =>
+      ControlAnimationWithConditionState();
+}
+
+class ControlAnimationWithConditionState
+    extends AnimationControllerState<ControlAnimationWithCondition> {
+  @override
+  Duration get duration => widget.duration;
+  @override
+  Widget build(BuildContext context) {
+    if (widget.condition) {
+      animationController.forward();
+    } else {
+      animationController.reverse();
+    }
+    return AnimatedBuilder(
+      builder: (context, child) => widget.builder(
+        context,
+        child,
+        animationController,
+      ),
+      animation: animationController,
+      child: widget.child,
+    );
+  }
+}
