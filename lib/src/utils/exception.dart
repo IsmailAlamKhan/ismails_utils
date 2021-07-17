700a9ee2d923a79873a8d8e82b11bf75380671b7
Ismail <mdismailalamkhan@gmail.com>
2021-07-17 16:41:43 +0600

    Reverted f1b518aba165c9dbd1556b782f170973f4a26be2

--- lib/src/utils/exception.dart
+++ lib/src/utils/exception.dart
@@ -58,118 +58,124 @@ class IsmailException with IsmailLoggerMixin implements Exception {
     }
   }
 
-  /// returns the messege
-  @override
-  String toString() => message ?? '';
-}
-
-abstract class IsmailFirebaseException implements Exception {
-  String? message;
-  @override
-  String toString() => message ?? '';
-  String? get onInvalidEmail => null;
-  String? get unAuthDomain => null;
-  String? get oAuthCanceled => null;
-  String? get accountExistsWithDifferentCred => null;
-  String? get wrongPass => null;
-  String? get networkFailure => null;
-  String? get tooManyReqs => null;
-  String? get disabled => null;
-  String? get reLogin => null;
-  String? get emailExists => null;
-  String? get userNotFound => null;
-  String? get phoneExists => null;
-  String? get invalidPhone => null;
-  String? get invalidEmail => null;
-  String? get deleteOwnAcc => null;
-  String? get aborted => null;
-  String? get docExists => null;
-  String? get permDenied => null;
-  String? get unAuth => null;
-  String? get docNotFound => null;
-  String? get objNotFound => null;
-  String? get genericError => null;
-
   /// return IsmailException from FromFirebaseErrorCodes
-  IsmailFirebaseException.fromFirebaseErrorCodes(String errorCode) {
+  IsmailException.fromFirebaseErrorCodes(String errorCode) {
     switch (errorCode) {
       case 'invalid-email':
-        message = onInvalidEmail ?? 'The email is badly formatted.';
+        message = 'The email is badly formatted.';
         break;
       case 'unauthorized-domain':
-        message = unAuthDomain ?? 'This domain is not authorized for OAuth.';
+        message = 'This domain is not authorized for OAuth.';
         break;
       case 'popup-closed-by-user':
-        message = oAuthCanceled ?? 'Cancelled by user.';
+        message = 'Cancelled by user.';
         break;
       case 'account-exists-with-different-credential':
-        message = accountExistsWithDifferentCred ??
+        message =
             'You already have an account with this email but with different credential.';
         break;
       case 'wrong-password':
-        message = wrongPass ?? 'Invalid User credentials.';
+        message = 'Invalid User credentials.';
         break;
       case 'network-request-failed':
-        message = networkFailure ?? 'Please check your internet connection';
+        message = 'Please check your internet connection';
         break;
       case 'too-many-requests':
-        message = tooManyReqs ??
+        message =
             'You inserted wrong login credentials several times. Take a break please!';
         break;
       case 'user-disabled':
-        message = disabled ??
+        message =
             'Your account has been disabled or deleted. Please contact the system administrator.';
         break;
       case 'requires-recent-login':
-        message = reLogin ?? 'Please login again and try again!';
+        message = 'Please login again and try again!';
         break;
       case 'email-already-exists':
       case 'email-already-in-use':
-        message = emailExists ??
-            'Email address is already in use by an existing user.';
+        message = 'Email address is already in use by an existing user.';
         break;
       case 'user-not-found':
-        message = userNotFound ??
+        message =
             'We could not find user account associated with the email address or phone number.';
         break;
       case 'phone-number-already-exists':
-        message = phoneExists ??
-            'The phone number is already in use by an existing user.';
+        message = 'The phone number is already in use by an existing user.';
         break;
       case 'invalid-phone-number':
-        message =
-            invalidPhone ?? 'The phone number is not a valid phone number!';
+        message = 'The phone number is not a valid phone number!';
         break;
       case 'invalid-email  ':
-        message =
-            invalidEmail ?? 'The email address is not a valid email address!';
+        message = 'The email address is not a valid email address!';
         break;
       case 'cannot-delete-own-user-account':
-        message = deleteOwnAcc ?? 'You cannot delete your own user account.';
+        message = 'You cannot delete your own user account.';
         break;
       case 'aborted':
-        message = aborted ?? 'Aborted due to errors.';
+        message = 'Aborted due to errors.';
         break;
       case 'already-exists':
-        message = docExists ?? 'The document already exits.';
+        message = 'The document already exits.';
+        break;
+      case 'cancelled':
+        message = 'Cancelled.';
+        break;
+      case 'internal':
+        message = 'Internal Server Error.';
         break;
       case 'permission-denied':
-        message = permDenied ??
-            "You don't have sufficient permissions. Please login again";
+        message = "You don't have sufficient permissions. Please login again";
         break;
       case 'unauthenticated':
-        message = unAuth ?? 'Your session is expired Please relogin.';
+        message = 'Your session is expired Please relogin.';
         break;
       case 'not-found':
-        message = docNotFound ?? 'The Document is not found.';
+        message = 'The Document is not found.';
         break;
       case 'object-not-found':
-        message = objNotFound ?? 'Could not find the file or the photo.';
+        message = 'Could not find the file or the photo.';
         break;
       default:
-        message =
-            genericError ?? 'Oops! Something went wrong. Try again later.';
+        message = 'Oops! Something went wrong. Try again later.';
         break;
     }
+  }
+
+  /// returns the messege
+  @override
+  String toString() => message ?? '';
+  IsmailException.fromDataBaseException(DatabaseException databaseException) {
+    message = 'Something went wrong';
+    final code = databaseException.getResultCode();
+    final _closed =
+        'isDatabaseClosedError ${databaseException.isDatabaseClosedError()}\n';
+    final _isDup =
+        'isDuplicateColumnError ${databaseException.isDuplicateColumnError()}\n';
+    final _isNoTable =
+        'isNoSuchTableError ${databaseException.isNoSuchTableError()}\n';
+    final _isNotNullConstraint =
+        'isNotNullConstraintError ${databaseException.isNotNullConstraintError()}\n';
+    final _isOpenFailedError =
+        'isOpenFailedError ${databaseException.isOpenFailedError()}\n';
+    final _isReadOnlyError =
+        'isReadOnlyError ${databaseException.isReadOnlyError()}\n';
+    final _isSyntaxError =
+        'isSyntaxError ${databaseException.isSyntaxError()}\n';
+    final _isUniqueConstraintError =
+        'isUniqueConstraintError ${databaseException.isUniqueConstraintError()}\n';
+    final String _messege = 'Code = $code\n'
+        '$_closed'
+        '$_isDup'
+        '$_isNoTable'
+        '$_isNotNullConstraint'
+        '$_isOpenFailedError'
+        '$_isReadOnlyError'
+        '$_isSyntaxError'
+        '$_isUniqueConstraintError';
+    logError(
+      'Sqllite Error',
+      error: _messege,
+      stackTrace: StackTrace.fromString(databaseException.toString()),
+    );
   }
 }
