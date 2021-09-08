e4501b71f97ebee92423ecb9dd0d770f53bda4c4
IsmailAlamKhan <mdismailalamkhan@gmail.com>
2021-09-08 19:09:06 +0600

    updated the AlphabetListViewModel for a better api

--- lib/src/widgets/alphabet_listview/model.dart
+++ lib/src/widgets/alphabet_listview/model.dart
@@ -3,27 +3,23 @@ import 'package:flutter/material.dart';
 import '../../src.dart';
 
 abstract class AlphabetListViewModel {
-  String get firstChar => name.length > 1
-      ? name[0].isAlphabetic
-          ? name[0]
-          : '#'
-      : '';
+  final String firstChar;
   final String name;
 
-  AlphabetListViewModel({required this.name});
+  AlphabetListViewModel({required this.name, String? firstChar})
+      : firstChar = firstChar ??
+            (name.length > 1
+                ? name[0].isAlphabetic
+                    ? name[0]
+                    : '#'
+                : '');
 
-  static List<AlphabetListViewModel> getListByFirstChar(
-    String char,
-    List<AlphabetListViewModel> list,
-  ) {
+  static List<AlphabetListViewModel> getListByFirstChar(String char, List<AlphabetListViewModel> list) {
     return list.where((e) => e.firstChar == char).toList();
   }
 }
 
 class StickyHeaderColor {
   final Color colorOne, colorTwo;
-  StickyHeaderColor({
-    required this.colorOne,
-    required this.colorTwo,
-  });
+  StickyHeaderColor({required this.colorOne, required this.colorTwo});
 }
